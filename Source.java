5b4f015bbe0946d0166f6885207eae08f4c08ace
Anupam <anusrivastava910@gmail.com>
2022-02-04 14:59:43 +0530

    ADD DUMP

---
+++ Source.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Salary {
+    HashMap<String, Integer> empList;
+    Salary() {
+        empList = new HashMap<>();
+    }
+
+    public int totalSalary() {
+        return empList.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public String getSalary(String designation) {
+        if(empList.containsKey(designation)) {
+            return "salary is " + empList.get(designation);
+        }
+        return "no designation match";
+    }
+
+    public void updateSalary(String designation, int newSalary) {
+        empList.put(designation, newSalary);
+    }
+}
+
+public class Source {
+    public static void main(String[] args) throws Exception{
+        Salary obj = new Salary();
+        obj.empList.put("CEO", 20000);
+        obj.empList.put("DEV", 5000);
+
+        System.out.println(obj.totalSalary());
+        obj.updateSalary("DEV", 6000);
+        System.out.println(obj.getSalary("DEV"));
+    }
+}
